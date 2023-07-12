16dae2445f66719553e252990f2f743b7772e32f
yudai2929 <yudai201729@icloud.com>
2023-07-12 19:25:42 +0900

    add: スワイプ機能を追加

--- lib/src/screens/home/screem.dart
+++ lib/src/screens/home/screem.dart
@@ -1,5 +1,7 @@
 import 'package:cheese_client/src/components/ui/header.dart';
 import 'package:flutter/material.dart';
+import 'package:appinio_swiper/appinio_swiper.dart';
+import 'package:flutter/cupertino.dart';
 
 const crossImagePath = 'assets/images/cross.png';
 const heartImagePath = 'assets/images/heart.png';
@@ -15,21 +17,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool _canSwipe = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: const Header(title: ""),
-        body: Container(
-          padding: const EdgeInsets.all(8),
-          width: double.infinity,
-          child: Card(
+      appBar: const Header(title: ""),
+      body: AppinioSwiper(
+        cardsCount: 10,
+        backgroundCardsCount: 1,
+        // isDisabled: !_canSwipe,
+        allowUnswipe: _canSwipe,
+        padding: const EdgeInsets.all(16.0),
+        onSwipe: (int index, AppinioSwiperDirection direction) {
+          print(direction.toString());
+        },
+        onSwiping: (AppinioSwiperDirection direction) {
+          final isSwipingRight = direction == AppinioSwiperDirection.right;
+          final isSwipingLeft = direction == AppinioSwiperDirection.left;
+          setState(() {
+            _canSwipe = isSwipingRight || isSwipingLeft;
+          });
+        },
+        cardsBuilder: (BuildContext context, int index) {
+          return Card(
             shape: const RoundedRectangleBorder(
               // アンダーラインを引く
               borderRadius: BorderRadius.all(Radius.circular(16.0)),
             ),
             color: Colors.white,
             child: Padding(
-              padding: EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(16.0),
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -100,7 +118,9 @@ class _HomeScreenState extends State<HomeScreen> {
                     )
                   ]),
             ),
-          ),
-        ));
+          );
+        },
+      ),
+    );
   }
 }
